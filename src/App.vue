2d6748402d0a4f7125454444643701782b89e209
hiroki.hayashi <hiroki.hayashi@booklive.co.jp>
2019-06-24 19:14:27 +0900

    add delete button fix layout

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,25 @@
 <template lang="pug">
   .container
-    .columns
+    b-button(@click="addForm").is-success.is-medium.main-field add
+    .columns.is-multiline
       draggable(
         v-model="forms"
         v-bind="dragOption"
         @start="drag = true"
         @end="drag = false"
-      ).main-field.column.is-three-fifths.is-offset-one-fifth
+      ).main-field.column.is-full
         transition-group(
           type="transition"
           :name="!drag ? 'flip-list' : null"
         )
-          .box(v-for="(element, index) in forms" :key="index")
+          .box(v-for="(el, i) in forms" :key="i")
             b-field
-              b-select(placeholder="Select a name" v-model="formType")
+              b-select(placeholder="Select a name"  v-model="el.formType")
                 option(v-for="formType in formTypes") {{ formType }}
-              b-checkbox.check-required(v-model="element.isRequired") required
+              b-checkbox.check-required(v-model="el.isRequired") required
+              b-button(@click="delForm(i)").is-danger.del-form delete
             b-field
-              b-input(v-model="element.title" placeholder="form title")
-      a(@click="addForm").button.is-success.is-medium add
+              b-input(v-model="el.title" placeholder="form title")
     pre {{ forms }}
 </template>
 
@@ -65,6 +66,9 @@ export default class Index extends Vue {
     const maxVal = Math.max.apply(null, formId);
     this.forms.push({ id: maxVal + 1, title: '', formType: 'input', isRequired: false });
   }
+
+  // public delForm(index) {
+  // }
 }
 </script>
 
@@ -76,6 +80,10 @@ export default class Index extends Vue {
 
 .check-required {
   margin-left: 30px;
+}
+
+.del-form {
+  margin-left: 20px;
 }
 
 </style>
