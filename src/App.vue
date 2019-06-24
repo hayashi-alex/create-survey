0750a95c96bb86f6e63be00b058bc93cc41ea2a3
hiroki.hayashi <hiroki.hayashi@booklive.co.jp>
2019-06-24 18:15:20 +0900

    add transition

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template lang="pug">
   .container
     .columns
-      draggable(v-model="forms").main-field.column.is-three-fifths.is-offset-one-fifth
-        .box(v-for="(element, index) in forms" :key="index")
-          b-field
-            b-select(placeholder="Select a name")
-              option(v-for="formType in formTypes") {{ formType }}
-            b-checkbox.check-required(v-model="element.isRequired") required
-          b-field
-            b-input(v-model="element.title" placeholder="form title")
-        a(@click="addForm").button.is-success.is-medium add
+      draggable(
+        v-model="forms"
+        v-bind="dragOption"
+        @start="drag = true"
+        @end="drag = false"
+      ).main-field.column.is-three-fifths.is-offset-one-fifth
+        transition-group(
+          type="transition"
+          :name="!drag ? 'flip-list' : null"
+        )
+          .box(v-for="(element, index) in forms" :key="index")
+            b-field
+              b-select(placeholder="Select a name" v-model="formType")
+                option(v-for="formType in formTypes") {{ formType }}
+              b-checkbox.check-required(v-model="element.isRequired") required
+            b-field
+              b-input(v-model="element.title" placeholder="form title")
+      a(@click="addForm").button.is-success.is-medium add
     pre {{ forms }}
 </template>
 
@@ -33,6 +42,15 @@ export default class Index extends Vue {
     },
   ];
 
+  private dragOption: { animation: number; group: string; disabled: boolean; ghostClass: string;}[] = [
+    {
+      animation: 200,
+      group: "description",
+      disabled: false,
+      ghostClass: "ghost"
+    }
+  ]
+
   private formTypes: string[] = [
     'input',
     'email',
@@ -45,7 +63,7 @@ export default class Index extends Vue {
   public addForm() {
     const formId: number[] = this.forms.map((x) => x.id);
     const maxVal = Math.max.apply(null, formId);
-    this.forms.push({ id: maxVal + 1, title: '', formType: '', isRequired: false });
+    this.forms.push({ id: maxVal + 1, title: '', formType: 'input', isRequired: false });
   }
 }
 </script>
